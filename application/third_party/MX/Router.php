1f0ff1dfc215e55d9ee5039fc687fe7cd8ab46a3
Paolo Rampino <liadserv@gmail.com>
2020-03-12 17:30:08 +0100

    Fix MX to work with php7.4

--- application/third_party/MX/Router.php
+++ application/third_party/MX/Router.php
@@ -234,12 +234,15 @@ class MX_Router extends CI_Router
 	}
 
 	public function set_class($class)
-	{
-		$suffix = $this->config->item('controller_suffix');
-		if (strpos($class, $suffix) === FALSE)
 		{
-			$class .= $suffix;
+		//  $suffix = $this->config->item('controller_suffix');
+    		//  if (strpos($class, $suffix) === FALSE)
+
+		$suffix = (string) $this->config->item('controller_suffix');
+		if ($suffix && strpos($class, $suffix) === FALSE)
+			{
+				$class .= $suffix;
+			}
+			parent::set_class($class);
 		}
-		parent::set_class($class);
-	}
 }	
